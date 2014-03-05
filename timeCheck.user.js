fd252ad9636e329b7a6134fc984e5dc2aee4b4d0
languantan <languan.tan@gmail.com>
2014-03-05 18:17:41 +0800

    Create timeCheck.user.js

---
+++ timeCheck.user.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name       Facebook Time Check
+// @namespace  http://github.com/languantan/stop-fb-loitering
+// @updateURL  https://raw.github.com/languantan/stop-fb-loitering/master/timeCheck.user.js
+// @downloadURL https://raw.github.com/languantan/stop-fb-loitering/master/timeCheck.user.js
+// @version    0.1
+// @description  stop user from spending too much time on facebook
+// @match      https://www.facebook.com/*
+// @copyright  2012+, languantan
+// ==/UserScript==
+
+
+function timeScript(){
+      
+    window.lgtan = {};
+    window.lgtan.timeCheck = function() {
+    	var timeout, logoutForm;
+        
+        //keep trying to find logoutForm
+        var findLO = function(){
+            logoutForm = document.getElementById('logout_form');
+            if (logoutForm){
+                console.log('found logoutForm');
+                main();
+            } else {
+                console.log('not logged in');
+            }
+        }
+        
+       var main = function(){
+            
+            timeout = sessionStorage.getItem('fbtime');
+            if (!timeout){
+                timeout =  prompt('How long do you wanna stay on Facebook?', 'in minutes') * 60000;
+                sessionStorage.setItem('fbtime', timeout);
+            }
+            setInterval(timerAction, 5000);
+        }
+        
+        var timerAction = function(){
+            console.log(timeout + " milliseconds left");
+            timeout -= 5000;
+            if (timeout > 0){
+                sessionStorage.setItem('fbtime', timeout);
+            } else {
+                logoutAction();
+            }
+        }
+        
+        var logoutAction = function(){
+            console.log('Logging out');
+            alert('your personally defined time is up');
+            sessionStorage.removeItem('fbtime');
+            logoutForm.submit();   
+        }
+        
+        findLO();
+    }
+    
+    window.lgtan.timeCheck();
+    
+}
+
+if (window.top==window.self){
+    var script = document.createElement('script');
+    script.appendChild(document.createTextNode('('+ timeScript +')();'));
+    (document.body || document.head || document.documentElement).appendChild(script);
+}
+
+
+
